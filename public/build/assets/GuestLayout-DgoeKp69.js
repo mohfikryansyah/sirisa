6837926f543774b481ca4a2c802b172f41c2b040
mohfikryansyah <moh.fikryansyah@gmail.com>
2025-02-16 01:38:30 +0800

    Add new asset files and remove deprecated ones for improved functionality

--- public/build/assets/GuestLayout-DgoeKp69.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-BLQvmwDf.js";function e({children:s}){return t.jsx("div",{className:"w-full",children:s})}export{e as G};
--- public/build/assets/index-uYqOO2L-.js
+++
@@ -1,1 +0,0 @@
-import{b as r,g as o}from"./app-BLQvmwDf.js";var t=r();const e=o(t);export{e as $,t as r};
