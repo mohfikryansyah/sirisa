fac695dd7ebbb5ffbbc41edf2d9df02e0b53b266
mohfikryansyah <moh.fikryansyah@gmail.com>
2025-02-28 15:58:25 +0800

    Comment out Google authentication routes for future reference

--- routes/auth.php
+++ routes/auth.php
@@ -38,37 +38,37 @@ Route::middleware('guest')->group(function () {
     Route::post('reset-password', [NewPasswordController::class, 'store'])
         ->name('password.store');
 
-    Route::get('auth/google', function () {
-        return Socialite::driver('google')->redirect();
-    })->name('auth.google');
+    // Route::get('auth/google', function () {
+    //     return Socialite::driver('google')->redirect();
+    // })->name('auth.google');
 
-    Route::get('auth/google/callback', function () {
-        try {
-            $googleUser = Socialite::driver('google')->user();
-            $user = User::where('email', $googleUser->email)->first();
+    // Route::get('auth/google/callback', function () {
+    //     try {
+    //         $googleUser = Socialite::driver('google')->user();
+    //         $user = User::where('email', $googleUser->email)->first();
 
-            if ($user) {
-                if (!$user->google_id) {
-                    $user->update([
-                        'google_id' => $googleUser->id,
-                        'avatar' => $googleUser->avatar,
-                    ]);
-                }
-            } else {
-                User::create([
-                    'google_id' => $googleUser->id,
-                    'name' => $googleUser->name,
-                    'email' => $googleUser->email,
-                    'avatar' => $googleUser->avatar,
-                    'password' => Hash::make("password" . $googleUser->id . time()),
-                ]);
-            }
-            Auth::login($user);
-            return redirect('/dashboard');
-        } catch (\Exception $e) {
-            return redirect('/login')->with('error', 'Terjadi kesalahan saat login menggunakan Google');
-        }
-    })->name('auth.google.callback');
+    //         if ($user) {
+    //             if (!$user->google_id) {
+    //                 $user->update([
+    //                     'google_id' => $googleUser->id,
+    //                     'avatar' => $googleUser->avatar,
+    //                 ]);
+    //             }
+    //         } else {
+    //             User::create([
+    //                 'google_id' => $googleUser->id,
+    //                 'name' => $googleUser->name,
+    //                 'email' => $googleUser->email,
+    //                 'avatar' => $googleUser->avatar,
+    //                 'password' => Hash::make("password" . $googleUser->id . time()),
+    //             ]);
+    //         }
+    //         Auth::login($user);
+    //         return redirect('/dashboard');
+    //     } catch (\Exception $e) {
+    //         return redirect('/login')->with('error', 'Terjadi kesalahan saat login menggunakan Google');
+    //     }
+    // })->name('auth.google.callback');
 });
 
 Route::middleware('auth')->group(function () {
