3119ff842594703bdd0f1dcaa25bd6f442ba22ab
mohfikryansyah <moh.fikryansyah@gmail.com>
2025-08-27 12:23:40 +0800

    Fix email notification for complaint status in store method

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -80,7 +80,7 @@ class ComplaintController extends Controller
                 }
             }
 
-            // Mail::to('nursepthyarazak@gmail.com')->send(new ComplaintStatusMail($complaint));
+            Mail::to('nursepthyarazak@gmail.com')->send(new ComplaintStatusMail($complaint));
 
             DB::commit();
             return redirect()->back();
